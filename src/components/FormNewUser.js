8709437a6a77ee2c6208181f6a7e53b2592f8d67
Priscila Fernandes <priscila.fernandes@maxxidata.com>
2020-01-20 14:13:00 -0300

    estilização dos botões

--- src/components/FormNewUser.js
+++ src/components/FormNewUser.js
@@ -9,15 +9,15 @@ class FormNewUser extends Component {
   
   render() {
     return (
-      <div className='container'>
+      <Row className='container'>
         <Row type='flex' justify='center'>
-          <Col span={9} className='img-backg img-block'>
-            <h1>Hello World</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.</p>
+          <Col span={10} className='img-backg img-block'>
+            <h1 className='title'>Hello<br/>World.</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br/>Aenean euismod bibendum laoreet.</p>
           </Col>
 
-          <Col span={9} className='form-block'>
-            <h1>Register</h1>
+          <Col span={10} className='form-block'>
+            <h1 className='subtitle'>Register</h1>
             <p>Don't have an account? <a href='#'>Create your account</a>, it takes less than a minute.</p>
             
             <Form>
@@ -26,20 +26,30 @@ class FormNewUser extends Component {
               <Form.Item label='PHONE'><Input /></Form.Item>
               <Form.Item label='PASSWORD'><Input type='password' /></Form.Item>
               <Form.Item>
-                <Checkbox>I accept <a href='#'>terms and conditions</a> and <a href='#'>privacy policy</a>.</Checkbox>
+                <Checkbox className='checkbox'>I accept <a href='#'>terms and conditions</a> and <a href='#'>privacy policy</a>.</Checkbox>
               </Form.Item>
-              <Form.Item><Button type='primary' htmlType='submit'>Login</Button></Form.Item>
+              <Form.Item>
+                <Row type='flex' justify='end'>
+                  <Button type='primary' htmlType='submit' className='btn-login'>LOGIN</Button>
+                </Row>
+              </Form.Item>
             </Form>
 
-            <div>
-              <p>Login with social media</p>
-              <Button type='button'><Icon type="facebook" theme="filled" />Facebook</Button>
-              <Button type='button'><Icon type="twitter-square" theme="filled" />Twitter</Button>
-              <Button type='button'><Icon type="google-plus-square" theme="filled" />Google +</Button>
-            </div>
+            <p>Login with social media</p>
+            <Row type='flex' justify='space-between'>
+              <Button type='button' className='btn-social facebook'>
+                <Icon type='facebook' theme='filled' />Facebook
+              </Button>
+              <Button type='button' className='btn-social twitter'>
+                <Icon type='twitter-square' theme='filled' />Twitter
+              </Button>
+              <Button type='button' className='btn-social google'>
+                <Icon type='google-plus-square' theme='filled' />Google +
+              </Button>
+            </Row>
           </Col>
         </Row>
-      </div>
+      </Row>
     );
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -17,5 +17,24 @@ module.exports = {
         { loader: 'css-loader' }
       ]
     },
+    {
+      test: /\.less$/,
+      use: [
+        {
+          loader: "style-loader"
+        },
+        {
+          loader: "css-loader",
+          options: {
+            sourceMap: true,
+            modules: true,
+            localIdentName: "[local]___[hash:base64:5]"
+          }
+        },
+        {
+          loader: "less-loader"
+        }
+      ]
+    }
   ]
 }
